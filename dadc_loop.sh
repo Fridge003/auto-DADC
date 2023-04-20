44bf8c792447653b35757d75d18423080f901ac6
Fridge003 <eddiezhang@pku.edu.cn>
2023-04-19 17:31:31 -0700

    fix dadc_loop.sh

--- dadc_loop.sh
+++ dadc_loop.sh
@@ -19,6 +19,10 @@ test_device=0
 # Total number of loops for DADC.
 total_loop=10
 
+# Checkpointing. The dadc loop will start from starting_loop.
+# Under default setting, starting_loop will be set to 0 to start from empty workspace.
+starting_loop=0
+
 # This should be the main working directory that stores following files:
 # 1. All the collected data through prompting GPT
 # 2. All the models fine-tuned at each cycle.
@@ -56,96 +60,108 @@ predict_batch_size=32
 
 # --------------------------------2. Run Script----------------------------------------
 
-mkdir -p $working_dir
+
 all_models_path="$working_dir/models"
 all_collected_data_path="$working_dir/collected_data"
-mkdir -p $all_models_path
-mkdir -p $all_collected_data_path
-
 result_record="$working_dir/prediction_result.json"
 
-# First test the performance of starting model.
-python predict.py \
-    --model_name_or_path $base_model \
-    --num_labels $num_labels \
-    --test_data_path $test_data_path \
-    --result_path $result_record \
-    --predict_batch_size $predict_batch_size \
-    --device $test_device \
-    --num_labels $num_labels \
-    --loop_cnt 0
-
-
-# Start DADC Loop
+# Default setting: starting from base model
 loop_cnt=1
 last_model_path="$base_model"
 last_data_path="Init"
 
+if  [[ $starting_loop -eq 0 ]]
+then
+    mkdir -p $working_dir
+    mkdir -p $all_models_path
+    mkdir -p $all_collected_data_path
+
+    # First test the performance of starting model.
+    python predict.py \
+        --model_name_or_path $base_model \
+        --num_labels $num_labels \
+        --test_data_path $test_data_path \
+        --result_path $result_record \
+        --predict_batch_size $predict_batch_size \
+        --device $test_device \
+        --num_labels $num_labels \
+        --loop_cnt 0
+
+else 
+    # Starting from Checkpoint
+    loop_cnt=$starting_loop
+    last_loop=$((starting_loop-1))
+    last_model_path="$all_models_path/finetuned_model_$last_loop"
+    last_data_path="$all_collected_data_path/collected_data_$last_loop"
+fi
+
+
+
 while [ $loop_cnt -le $total_loop ]
 do
 
-# Create the folder that stores model fine-tuned at this loop.
-next_model_path="$all_models_path/finetuned_model_$loop_cnt"
-next_data_path="$all_collected_data_path/collected_data_$loop_cnt"
-mkdir -p $next_model_path
-mkdir -p $next_data_path
+    # Create the folder that stores model fine-tuned at this loop.
+    next_model_path="$all_models_path/finetuned_model_$loop_cnt"
+    next_data_path="$all_collected_data_path/collected_data_$loop_cnt"
+    mkdir -p $next_model_path
+    mkdir -p $next_data_path
 
-# Prompt ChatGPT for examples.
-OPENAI_API_KEY=$openai_api_key \
-python -m data_generation generate_data_by_prompting \
-    --input_dir $last_data_path \
-    --output_dir $next_data_path \
-    --seed_dataset_path  $seed_data_path \
-    --num_examples_to_generate $num_examples_generated_per_loop \
-    --validation_ratio 0.1 \
-    --model_name gpt-3.5-turbo \
-    --num_prompt_examples 3 \
-    --num_genetated_examples_per_prompt 5 \
-    --temperature 1.0 \
-    --num_cpus 8
+    # Prompt ChatGPT for examples.
+    OPENAI_API_KEY=$openai_api_key \
+    python -m data_generation generate_data_by_prompting \
+        --input_dir $last_data_path \
+        --output_dir $next_data_path \
+        --seed_dataset_path  $seed_data_path \
+        --num_examples_to_generate $num_examples_generated_per_loop \
+        --validation_ratio 0.1 \
+        --model_name gpt-3.5-turbo \
+        --num_prompt_examples 3 \
+        --num_genetated_examples_per_prompt 5 \
+        --temperature 1.0 \
+        --num_cpus 8
 
-# Finetune model on data collected.
-CUDA_VISIBLE_DEVICES=$cuda_visible_devices torchrun \
-    --nnodes=1 --nproc_per_node=$num_cuda_devices --master_port=1234 train.py \
-    --model_name_or_path $last_model_path \
-    --num_labels $num_labels \
-    --train_data_path "$next_data_path/gen_train.csv" \
-    --eval_data_path "$next_data_path/gen_validation.csv" \
-    --test_data_path $test_data_path \
-    --output_dir $next_model_path \
-    --num_train_epochs $num_epoch \
-    --per_device_train_batch_size $per_device_train_batch_size \
-    --per_device_eval_batch_size $per_device_eval_batch_size \
-    --evaluation_strategy "epoch" \
-    --save_strategy "epoch" \
-    --save_total_limit 1 \
-    --gradient_accumulation_steps 4 \
-    --learning_rate 2e-5 \
-    --lr_scheduler_type "linear" \
-    --weight_decay 0. \
-    --warmup_ratio 0.1 \
-    --logging_steps 1 \
-    --load_best_model_at_end True \
-    --fsdp "full_shard auto_wrap" \
-    --early_stopping_patience $early_stopping_patience \
-    --metric_for_best_model "accuracy"
+    # Finetune model on data collected.
+    CUDA_VISIBLE_DEVICES=$cuda_visible_devices torchrun \
+        --nnodes=1 --nproc_per_node=$num_cuda_devices --master_port=1234 train.py \
+        --model_name_or_path $last_model_path \
+        --num_labels $num_labels \
+        --train_data_path "$next_data_path/gen_train.csv" \
+        --eval_data_path "$next_data_path/gen_validation.csv" \
+        --test_data_path $test_data_path \
+        --output_dir $next_model_path \
+        --num_train_epochs $num_epoch \
+        --per_device_train_batch_size $per_device_train_batch_size \
+        --per_device_eval_batch_size $per_device_eval_batch_size \
+        --evaluation_strategy "epoch" \
+        --save_strategy "epoch" \
+        --save_total_limit 0 \
+        --gradient_accumulation_steps 4 \
+        --learning_rate 2e-5 \
+        --lr_scheduler_type "linear" \
+        --weight_decay 0. \
+        --warmup_ratio 0.1 \
+        --logging_steps 1 \
+        --load_best_model_at_end True \
+        --fsdp "full_shard auto_wrap" \
+        --early_stopping_patience $early_stopping_patience \
+        --metric_for_best_model "accuracy"
 
 
-# Test performance on newly-finetuned model.
-python predict.py \
-    --model_name_or_path $next_model_path \
-    --num_labels $num_labels \
-    --test_data_path $test_data_path \
-    --result_path $result_record \
-    --predict_batch_size $predict_batch_size \
-    --device $test_device \
-    --loop_cnt $loop_cnt
+    # Test performance on newly-finetuned model.
+    python predict.py \
+        --model_name_or_path $next_model_path \
+        --num_labels $num_labels \
+        --test_data_path $test_data_path \
+        --result_path $result_record \
+        --predict_batch_size $predict_batch_size \
+        --device $test_device \
+        --loop_cnt $loop_cnt
 
-# Update State
-last_model_path="$next_model_path"
-last_data_path="$next_data_path"
+    # Update State
+    last_model_path="$next_model_path"
+    last_data_path="$next_data_path"
 
-((loop_cnt++))
+    ((loop_cnt++))
 
 done
 
